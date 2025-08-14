94b3d57103a399e13cad2c95fe7a922e6b59bf63
CasherNZ <rsperformance@hotmail.com>
2025-08-14 20:34:38 +1200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   root: path.resolve(__dirname, "client"),
-  build: {
-    outDir: path.resolve(__dirname, "dist"),
-    emptyOutDir: true,
+  build: { outDir: path.resolve(__dirname, "dist"), emptyOutDir: true },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "client/src"),
+      "@components": path.resolve(__dirname, "client/src/components"),
+      "@shared": path.resolve(__dirname, "shared"),
+    },
   },
 });
